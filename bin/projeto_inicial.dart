d58d8dd7f22b94eb147e4a2661577df66ec3e0e6
luis-toledo <luis.toledo@aluno.ufr.edu.br>
2023-09-15 13:04:17 -0400

    first commit

---
+++ bin/projeto_inicial.dart
@@ -0,0 +1,9 @@
+import 'package:projeto_inicial/projeto_inicial.dart' as projeto_inicial;
+
+
+void main(List<String> arguments) {
+  
+  List<String> listanomes = ['Luis', 'Jader', 'Guilherme'];
+  List<dynamic> luis = [27, 1.91, true, 'Luís Fernando Toledo', 'Cabeça'];
+  print(listanomes);
+}
